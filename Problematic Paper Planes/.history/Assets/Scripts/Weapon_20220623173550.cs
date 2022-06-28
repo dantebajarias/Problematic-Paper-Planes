b128d686909867eeeb6e7a7b6612fc1502c779ee
dantebajarias <56666947+dantebajarias@users.noreply.github.com>
2022-06-28 15:17:52 -0700

    Delete script history

--- Problematic Paper Planes/.history/Assets/Scripts/Weapon_20220623173550.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
-public class Weapon : ScriptableObject
-{
-    
-}
